278100b3dc15e96bd39793a522b6eef7b9af733b
gooyle <gooyle@163.com>
2018-07-20 11:20:36 +0800

    simple usart test
    for uint16_t using sendate and receivedate function

--- text_gy_stm32f407/USER/main.c
+++ text_gy_stm32f407/USER/main.c
@@ -8,19 +8,24 @@
 ********************************************************************************************************/ 
 #include "rcc_config.h"
 #include "pwm_init.h"
+#include "usart.h"
+#include "delay.h"
 
 int main(void)
 {
 	int inc = 0;
+	uint16_t res_gy = 0;//接收数据寄存器；
+	uart_init(115200);//波特率配置；
 	RCC_Config();
 	PWMConfig(10000-1,168-1);//84MHZ/(10000*168) = 50HZ
-	PWM_Enable();
+	PWM_Enable();//pwm使能
+	delay_init(168);//SYSTEMCLOCK 为168Mhz；
 	
-	//while(1)
+	while(1)
 	{
 		Angle(0,LF_J1);
-		Angle(0,ARM_J2);
+		Angle(0,ARM_J1);
 	}
-	while(1);
+	//while(1);
 	return 0;
 }
