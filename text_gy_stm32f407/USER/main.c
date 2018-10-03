cc90e72468ab49787ce094cb6f63f08246e2829a
gooyle <gooyle@163.com>
2018-10-03 21:08:08 +0800

    2018-10-3
    have some problems

--- text_gy_stm32f407/USER/main.c
+++ text_gy_stm32f407/USER/main.c
@@ -10,6 +10,7 @@
 #include "pwm_init.h"
 #include "usart_gy.h"
 #include "delay.h"
+//#include "stdio.h"
 
 int main(void)
 {
@@ -22,6 +23,14 @@ int main(void)
 	
 	/***********************----Á÷³Ìº¯Êý-----***********************************/
 	
-
+	printf("12.99\n");
+	delay_ms(100);
+	while(1)
+	{
+		Angle(0,LF_J1);
+		//delay_ms(500);
+		//Angle(-45,LF_J1);
+		//delay_ms(2000);
+	}
 	return 0;
 }
